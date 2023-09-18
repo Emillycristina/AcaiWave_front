22dfdc671765e49261d9af08e4f4edb7b166327c
Emilly Cristina <emilly.c.f@hotmail.com>
2023-09-17 21:15:48 -0300

    update:navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Grid from '@mui/material/Grid';
-import {useMediaQuery, useTheme, List, ListItem, ListItemText, Link, Drawer } from '@mui/material/'
+import {useMediaQuery, useTheme, List, ListItem, ListItemText, Drawer, Typography } from '@mui/material/'
 import Logo from '../../assets/açai1.png'
 import { AiOutlineMenu } from "react-icons/ai";
 import { useLocation } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import MuiLink from '@mui/material/Link';
+
 
 const Navbar = () => {
   const theme = useTheme()
@@ -26,66 +28,82 @@ const Navbar = () => {
     if (location.pathname !== '/' && location.pathname !== '/CriarConta') {
       return (
         <List>
-          <ListItem component={Link} to="/" onClick={toggleDrawer}>
-            <ListItemText secondary="ENTRAR" style={{ color: '#fff', textDecoration: 'none', cursor: 'pointer' }} />
-          </ListItem>
-          <ListItem component={Link} to="/Home" onClick={toggleDrawer}>
-            <ListItemText secondary="HOME" style={{ textDecoration: 'none', cursor: 'pointer' }} />
-          </ListItem>
-          <ListItem component={Link} to="/Pedidos" onClick={toggleDrawer}>
+        <ListItem>
+          <MuiLink component={RouterLink} to="/Pedidos" onClick={toggleDrawer}>
             <ListItemText secondary="PEDIDOS" style={{ textDecoration: 'none', cursor: 'pointer' }} />
-          </ListItem>
-          <ListItem component={Link} to="/CriarConta" onClick={toggleDrawer}>
-            <ListItemText secondary="CADASTRAR" style={{ textDecoration: 'none', cursor: 'pointer' }} />
-          </ListItem>
-          <ListItem component={Link} to="/" onClick={toggleDrawer}>
+          </MuiLink>
+        </ListItem>
+        <ListItem>
+          <MuiLink component={RouterLink} to="/PedidoStatus" onClick={toggleDrawer}>
+            <ListItemText secondary="STATUS PEDIDOS" style={{ textDecoration: 'none', cursor: 'pointer' }} />
+          </MuiLink>
+        </ListItem>
+       <ListItem>
+          <MuiLink component={RouterLink} to="/Home" onClick={toggleDrawer}>
+            <ListItemText secondary="MONTE SEU PEDIDO" style={{ textDecoration: 'none', cursor: 'pointer' }} />
+          </MuiLink>
+        </ListItem>
+        <ListItem>
+          <MuiLink component={RouterLink} to="/" onClick={toggleDrawer}>
             <ListItemText secondary="SAIR" style={{ textDecoration: 'none', cursor: 'pointer' }} />
-          </ListItem>
-        </List>
+          </MuiLink>
+        </ListItem>
+      </List>
+      
       );
     }
     return null; // Não renderize os links nas rotas "/" e "/CriarConta"
   };
 
-  
+  if (location.pathname === '/' || location.pathname === '/CriarConta') {
+    return null;
+  }
+
+ 
+
+
+
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#35155D', height: '4rem' }}>
-    <Toolbar sx={{ display: 'flex' }}>
+    <AppBar position="static" style={{ backgroundColor: '#35155D', height: '4rem' }}>
+    <Toolbar style={{ display: 'flex' }}>
       <Grid container>
         <Grid item>
           <img src={Logo} alt="logo" width={100} height={100} />
         </Grid>
       </Grid>
       {isMobile ? (
-        <IconButton edge="start" color="inherit" aria-label="menu"  onClick={toggleDrawer}>
-          <AiOutlineMenu sx={{ cursor: 'pointer' }} />
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+          <AiOutlineMenu style={{ cursor: 'pointer' }} />
         </IconButton>
       ) : (
-        <Grid container spacing={1} >
-          <Grid item sx={{ marginLeft: 'auto' }}>
-            <Typography variant='body' sx={{ color: '#FFF', marginRight: '2rem', cursor: 'pointer' }}>
-              Home
-            </Typography>
+        <Grid container spacing={1}>
+          <Grid item style={{ marginLeft: 'auto' }}>
+            <RouterLink to="/Home"  >
+              <Typography variant='body' sx={{color: "#FFF", textDecoration:"none", marginRight:'4px' }}>
+                Monte Seu Pedido
+              </Typography>
+            </RouterLink>
           </Grid>
           <Grid item>
-            <Typography variant='body' sx={{ color: '#FFF', cursor: 'pointer', marginRight: '2rem' }}>
-              Pedidos
+            <RouterLink to="/Pedidos" >
+            <Typography variant='body' sx={{color: "#FFF", textDecoration:"none", marginRight:'4px'}}>
+                Pedidos
             </Typography>
+            </RouterLink>
           </Grid>
-          <Grid item>
-            <Typography variant='body' sx={{ color: '#FFF', cursor: 'pointer', marginRight: '2rem' }}>
-            Cadastrar
+         <Grid item>
+            <RouterLink to="/PedidoStatus">
+            <Typography variant='body' sx={{color: "#FFF", textDecoration:"none", marginRight:'4px' }}>
+                Status Pedido
             </Typography>
+            </RouterLink>
           </Grid>
-          <Grid item>
-            <Typography variant='body' sx={{ color: '#FFF', cursor: 'pointer', marginRight: '2rem' }}>
-              Entrar
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant='body' sx={{ color: '#FFF', cursor: 'pointer' }}>
-              Sair
-            </Typography>
+           <Grid item>
+            <RouterLink to="/" >
+            <Typography variant='body' sx={{color: "#FFF", textDecoration:"none", marginRight:'4px' }}>
+                Sair
+              </Typography>
+            </RouterLink>
           </Grid>
         </Grid>
       )}
@@ -94,7 +112,6 @@ const Navbar = () => {
       {renderNavLinks()}
     </Drawer>
   </AppBar>
-
   )
 }
 
