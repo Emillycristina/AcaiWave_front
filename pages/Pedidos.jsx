2f50c0ab9143c5968985c392eeb6572939faad78
adrianalima99 <adriana.slima0899@gmail.com>
2023-09-18 22:20:12 -0300

    fix: ajustes

--- pages/Pedidos.jsx
+++ pages/Pedidos.jsx
@@ -1,13 +1,9 @@
-
-
 import Listagem from '../src/Components/Listagem/Listagem'
 import CarouselImage from '../src/Components/Corousel/CarouselImage'
-
 
 const Pedidos = () => {
   return (
     <div>
- 
      <CarouselImage />
      <Listagem />
     </div>
--- src/Components/StatusPedido/StatusPedido.jsx
+++ src/Components/StatusPedido/StatusPedido.jsx
@@ -1,130 +1,3 @@
-
-// import Table from "@mui/material/Table";
-// import { TableBody, TableContainer } from "@mui/material";
-// import { TableCell } from "@mui/material";
-// import { TableHead } from "@mui/material";
-// import { TableRow } from "@mui/material";
-// import { Paper } from "@mui/material";
-// import { Box } from "@mui/material";
-// import { styled } from "@mui/material";
-// import {Typography} from "@mui/material";
-// import { useEffect, useState } from "react";
-// import * as React from 'react';
-
-
-// const StyledTableRow = styled(TableRow)(({  }) => ({
-//   "&:nth-of-type(odd)": {
-//     backgroundColor: "#512B81",
-//     borderRadius:"25px",
-//   },
-//   "&:nth-of-type(even)": {
-//     backgroundColor: "#35155D",
-//   },
-// }));
-
-// const StyledLeftCell = styled(TableCell)(({  }) => ({
-//   borderRadius:"25px 0px 0px 25px"
-// }));
-
-// const StyledRightCell = styled(TableCell)(({  }) => ({
-//   borderRadius:"0px 25px 25px 0px"
-// }));
-
-// const StatusPedido = () => {
-//   const [produto, setProduto] = useState([])
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch('http://34.41.148.34/orders', {
-//         method: 'GET',
-//       } );
-//       setProduto(response.data);
-//   } catch (error) {
-//     // Lidar com erros de rede ou outros erros
-//     console.error('Erro ao fazer a solicitação:', error);
-//   }}
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <Typography
-//           variant="h5"
-//           component="div"
-//           sx={{ color: "#35155D", marginTop: 5, marginX: "auto", maxWidth: "300px", align:"center" }}
-//         >
-//           Acompanhe seus pedidos
-//         </Typography>
-//       <Box
-//         marginX={"auto"}
-//         maxWidth={"90%"}
-//         marginTop={"20px"}
-//         alignContent={"center"}
-//         sx={{ borderRadius: "25px", overflow: "hidden" }}
-//       >
-//         <TableContainer component={Paper} sx={{ backgroundColor: "#35155D" }}>
-//           <Table>
-//             <TableHead>
-//               <TableRow sx={{ borderRadius: "25px" }}>
-//                 <TableCell
-//                   align="center"
-//                   sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                 >
-//                   Número
-//                 </TableCell>
-//                 <TableCell
-//                   align="center"
-//                   sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                 >
-//                   Nome
-//                 </TableCell>
-//                 <TableCell
-//                   align="center"
-//                   sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                 >
-//                   Status
-//                 </TableCell>
-//               </TableRow>
-//             </TableHead>
-//             <TableBody>
-//               {produto.map((row) => (
-//                 <StyledTableRow key={row.numero}>
-//                   <StyledLeftCell
-//                     align="center"
-//                     component="th"
-//                     scope="row"
-//                     sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                   >
-//                     {row.numero}
-//                   </StyledLeftCell>
-
-//                   <TableCell
-//                     align="center"
-//                     sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                   >
-//                     {row.nome}
-//                   </TableCell>
-//                   <StyledRightCell
-//                     align="center"
-//                     sx={{ color: "#FFF", borderBottom: "none", fontSize:22 }}
-//                   >
-//                     {row.status}
-//                   </StyledRightCell>
-//                 </StyledTableRow>
-//               ))}
-//             </TableBody>
-//           </Table>
-//         </TableContainer>
-//       </Box>
-//     </div>
-//   );
-// };
-
-// export default StatusPedido;
-
-
 import React, { useEffect, useState } from "react";
 import {
   Table,
@@ -158,16 +31,16 @@ const StyledRightCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StatusPedido = () => {
-  const [produto, setProduto] = useState([]);
+  const [pedidos, setPedidos] = useState([]);
 
   const fetchData = async () => {
     try {
-      const response = await fetch("http://34.41.148.34/orders/findall", {
+      const response = await fetch("http://34.41.148.34/orders", {
         method: "GET",
       });
       if (response.ok) {
         const data = await response.json();
-        setProduto(data);
+        setPedidos(data);
       } else {
         console.error("Erro ao fazer a solicitação:", response.statusText);
       }
@@ -239,8 +112,8 @@ const StatusPedido = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {produto.map((row) => (
-                <StyledTableRow key={row.number_order}>
+              {pedidos.map((pedido) => (
+                <StyledTableRow key={pedido.number_order}>
                   <StyledLeftCell
                     align="center"
                     component="th"
@@ -251,7 +124,7 @@ const StatusPedido = () => {
                       fontSize: 22,
                     }}
                   >
-                    {row.customer_id}
+                    {pedido.number_order}
                   </StyledLeftCell>
 
                   <TableCell
@@ -262,8 +135,9 @@ const StatusPedido = () => {
                       fontSize: 22,
                     }}
                   >
-                    {row.status}
+                    {pedido.customer_id} {/* Exibe o número do cliente */}
                   </TableCell>
+                  
                   <StyledRightCell
                     align="center"
                     sx={{
@@ -272,7 +146,7 @@ const StatusPedido = () => {
                       fontSize: 22,
                     }}
                   >
-                    {row.status}
+                    {pedido.status}
                   </StyledRightCell>
                 </StyledTableRow>
               ))}
